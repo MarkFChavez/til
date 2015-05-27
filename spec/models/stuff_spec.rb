982242e138e20db9970b9ec1e67aa90aaa1da1fc
Mark Chavez <markchav3z@gmail.com>
2015-05-27 23:25:54 +0800

    Add test for validation

--- spec/models/stuff_spec.rb
+++ spec/models/stuff_spec.rb
@@ -1,5 +1,10 @@
 require "rails_helper"
 
 describe Stuff do
-
+  context "validations" do
+    it "requires content" do
+      stuff = Stuff.new
+      expect(stuff).not_to be_valid
+    end
+  end
 end
