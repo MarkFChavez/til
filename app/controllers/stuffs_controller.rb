1e4cd97a7ea199788838628b0ca290b6d0f41fdc
Mark Chavez <markchav3z@gmail.com>
2015-06-03 14:19:35 +0800

    Reverse results

--- app/controllers/stuffs_controller.rb
+++ app/controllers/stuffs_controller.rb
@@ -1,5 +1,5 @@
 class StuffsController < ApplicationController
   def index
-    @stuffs = Stuff.all.group_by { |s| s.created_at.beginning_of_day }.reverse
+    @stuffs = Stuff.all.group_by { |s| s.created_at.beginning_of_day }.sort_by { |k, _| k }.reverse
   end
 end
